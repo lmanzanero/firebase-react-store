9075b3ac21350aa0f643304a8c6f88fa36bc636b
Luis Manzanero <manzanero.luis1995@gmail.com>
2021-06-13 19:53:53 +0800

    added footer and subscribe button section

--- frontend/src/components/footer.js
+++ frontend/src/components/footer.js
@@ -2,8 +2,125 @@ import React from 'react'
 
 export default function Footer() {
   return (
-    <div>
-      Footers
-    </div>
+    <footer className="relative bg-gray-800 pt-8 pb-6">
+      <div
+        className="bottom-auto top-0 left-0 right-0 w-full absolute pointer-events-none overflow-hidden -mt-20"
+        style={{ height: "80px" }}
+      >
+         
+      </div>
+      <div className="container mx-auto px-4">
+        <div className="flex flex-wrap">
+          <div className="w-full lg:w-6/12 px-4">
+            <h4 className="text-3xl font-semibold text-gray-500">
+              Let's keep in touch!
+            </h4>
+            <h5 className="text-lg mt-0 mb-2 text-gray-500">
+              Find us on any of these platforms, we respond 1-2 business days.
+            </h5>
+            <div className="mt-6">
+              <button
+                className="bg-white text-blue-400 shadow-lg font-normal h-10 w-10 items-center justify-center align-center rounded-full outline-none focus:outline-none mr-2 p-3"
+                type="button"
+              >
+                <i className="flex fab fa-twitter"></i>
+              </button>
+              <button
+                className="bg-white text-blue-600 shadow-lg font-normal h-10 w-10 items-center justify-center align-center rounded-full outline-none focus:outline-none mr-2 p-3"
+                type="button"
+              >
+                <i className="flex fab fa-facebook-square"></i>
+              </button>
+              <button
+                className="bg-white text-pink-400 shadow-lg font-normal h-10 w-10 items-center justify-center align-center rounded-full outline-none focus:outline-none mr-2 p-3"
+                type="button"
+              >
+                <i className="flex fab fa-dribbble"></i>
+              </button>
+              <button
+                className="bg-white text-gray-900 shadow-lg font-normal h-10 w-10 items-center justify-center align-center rounded-full outline-none focus:outline-none mr-2 p-3"
+                type="button"
+              >
+                <i className="flex fab fa-github"></i>
+              </button>
+            </div>
+          </div>
+          <div className="w-full lg:w-6/12 px-4">
+            <div className="flex flex-wrap items-top mb-6">
+              <div className="w-full lg:w-4/12 px-4 ml-auto">
+                <span className="block uppercase text-gray-300 text-sm font-semibold mb-2">
+                  Useful Links
+                </span>
+                <ul className="list-unstyled">
+                  <li>
+                    <a className="text-gray-400 hover:text-gray-900 font-semibold block pb-2 text-sm"
+                        href="#">About Us
+                    </a>
+                  </li>
+                  <li>
+                    <a className="text-gray-400 hover:text-gray-900 font-semibold block pb-2 text-sm"
+                        href="#">Blog
+                    </a>
+                  </li>
+                  <li>
+                    <a className="text-gray-400 hover:text-gray-900 font-semibold block pb-2 text-sm"
+                        href="#">Github
+                    </a>
+                  </li>
+                  <li>
+                    <a className="text-gray-400 hover:text-gray-900 font-semibold block pb-2 text-sm"
+                        href="#">Free Products
+                    </a>
+                  </li>
+
+                </ul>
+              </div>
+              <div className="w-full lg:w-4/12 px-4">
+                <span className="block uppercase text-gray-300 text-sm font-semibold mb-2">
+                  Other Resources
+                </span>
+                <ul className="list-unstyled">
+                  <li>
+                    <a className="text-gray-400 hover:text-gray-900 font-semibold block pb-2 text-sm"
+                    href="#">MIT License
+                  </a>
+                  </li>
+                  <li>
+                    <a className="text-gray-400 hover:text-gray-900 font-semibold block pb-2 text-sm"
+                        href="#">Terms & Conditions
+                    </a>
+                  </li>
+                  <li>
+                    <a className="text-gray-400 hover:text-gray-900 font-semibold block pb-2 text-sm"
+                        href="#">Privacy Policy
+                    </a>
+                  </li>
+                  <li>
+                    <a className="text-gray-400 hover:text-gray-900 font-semibold block pb-2 text-sm"
+                        href="#">Contact Us
+                    </a>
+                  </li>
+                </ul>
+
+              </div>
+            </div>
+          </div>
+        </div>
+        <hr className="my-6 border-gray-400" />
+        <div className="flex flex-wrap items-center md:justify-between justify-center">
+          <div className="w-full md:w-4/12 px-4 mx-auto text-center">
+            <div className="text-sm text-gray-600 font-semibold py-1">
+              Copyright © {new Date().getFullYear()}{" "}-{" "}
+              <a
+                href="https://www.creative-tim.com"
+                className="text-gray-600 hover:text-gray-900"
+              >
+                Luis Manzanero
+              </a>.
+            </div>
+          </div>
+        </div>
+      </div>
+    </footer>
   )
 }
--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -1,38 +1,189 @@
-import * as React from "react" 
-import Layout from "../components/layouts/layout"
-import '../styles/global.css'
-
+import * as React from "react";
+import Layout from "../components/layouts/layout";
+import "../styles/global.css";
 
 // markup
 const IndexPage = () => {
   return (
-    <Layout> 
+    <Layout>
       <title>Firebase</title>
-      <main className="mt-10 mx-auto max-w-7xl px-4 sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 lg:px-8 xl:mt-28">
-        <div className="sm:text-center lg:text-left">
-          <h1 className="text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl">
-            <span className="block xl:inline">Data to enrich your</span>
-            <span className="block text-indigo-600 xl:inline">online business</span>
-          </h1>
-          <p className="mt-3 text-base text-gray-500 sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl lg:mx-0">
-            Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo. Elit sunt amet fugiat veniam occaecat fugiat aliqua.
-          </p>
-          <div className="mt-5 sm:mt-8 sm:flex sm:justify-center lg:justify-start">
-            <div className="rounded-md shadow">
-              <a href="#" className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 md:py-4 md:text-lg md:px-10">
-                Get started
-              </a>
-            </div>
-            <div className="mt-3 sm:mt-0 sm:ml-3">
-              <a href="#" className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-indigo-700 bg-indigo-100 hover:bg-indigo-200 md:py-4 md:text-lg md:px-10">
-                Live demo
-              </a>
+      <main className="mt-10 mx-auto max-w-7xl px-4 sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 lg:px-8 xl:mt-15">
+        <section className="relative py-10 bg-pink-200">
+          <div
+            className="bottom-auto top-0 left-0 right-0 w-full pointer-events-none -mt-20"
+            style={{ height: "80px" }}
+          >
+            <svg
+              className="absolute bottom-0 overflow-hidden"
+              xmlns="http://www.w3.org/2000/svg"
+              preserveAspectRatio="none"
+              version="1.1"
+              viewBox="0 0 2560 100"
+              x="0"
+              y="0"
+            >
+              <polygon
+                className="text-white fill-current"
+                points="2560 0 2560 100 0 100"
+              ></polygon>
+            </svg>
+          </div>
+
+          <div className="container mx-auto px-4">
+            <div className="items-center flex flex-wrap">
+              <div className="w-full md:w-4/12 ml-auto mr-auto px-4">
+                <img
+                  alt="..."
+                  className="max-w-full rounded-lg shadow-lg"
+                  src="https://images.unsplash.com/photo-1555212697-194d092e3b8f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80"
+                />
+              </div>
+              <div className="w-full md:w-5/12 ml-auto mr-auto px-4">
+                <div className="md:pr-12">
+                  <div className="text-pink-600 p-3 text-center inline-flex items-center justify-center w-16 h-16 mb-6 shadow-lg rounded-full bg-pink-300">
+                    <i className="fas fa-rocket text-xl"></i>
+                  </div>
+                  <h3 className="text-3xl font-semibold">A growing company</h3>
+                  <p className="mt-4 text-lg leading-relaxed text-gray-600">
+                    The extension comes with three pre-built pages to help you
+                    get started faster. You can change the text and images and
+                    you're good to go.
+                  </p>
+                  <ul className="list-none mt-6">
+                    <li className="py-2">
+                      <div className="flex items-center">
+                        <div>
+                          <span className="text-xs font-semibold inline-block py-1 px-2 uppercase rounded-full text-pink-600 bg-purple-200 mr-3">
+                            <i className="fas fa-fingerprint"></i>
+                          </span>
+                        </div>
+                        <div>
+                          <h4 className="text-gray-600">
+                            Carefully crafted components
+                          </h4>
+                        </div>
+                      </div>
+                    </li>
+                    <li className="py-2">
+                      <div className="flex items-center">
+                        <div>
+                          <span className="text-xs font-semibold inline-block py-1 px-2 uppercase rounded-full text-pink-600 bg-green-200 mr-3">
+                            <i className="fab fa-html5"></i>
+                          </span>
+                        </div>
+                        <div>
+                          <h4 className="text-gray-600">
+                            Amazing page examples
+                          </h4>
+                        </div>
+                      </div>
+                    </li>
+                    <li className="py-2">
+                      <div className="flex items-center">
+                        <div>
+                          <span className="text-xs font-semibold inline-block py-1 px-2 uppercase rounded-full text-pink-600 bg-blue-200 mr-3">
+                            <i className="far fa-paper-plane"></i>
+                          </span>
+                        </div>
+                        <div>
+                          <h4 className="text-gray-600">Dynamic components</h4>
+                        </div>
+                      </div>
+                    </li>
+                  </ul>
+                </div>
+              </div>
             </div>
           </div>
-        </div>
-      </main>  
-    </Layout>
-  )
-}
+        </section>
+        
+        <section className="pb-20 relative block">
+          <div
+            className="bottom-auto top-0 left-0 right-0 w-full"
+            style={{ height: "80px" }}
+          >
+          </div>
 
-export default IndexPage
+          <div className="container mx-auto px-4 lg:pt-14 lg:pb-10 ">
+            <div className="flex flex-wrap text-center justify-center">
+              <div className="w-full lg:w-6/12 px-4">
+                <h2 className="text-4xl font-semibold text-gray-500">
+                  Build something
+                </h2>
+                <p className="text-lg leading-relaxed mt-4 mb-4 text-gray-500">
+                  Put the potentially record low maximum sea ice extent tihs year down to low ice.
+                  According to the National Oceanic and Atmospheric Administration, Ted, Scambos.
+                </p>
+              </div>
+            </div>
+            <div className="flex flex-wrap mt-12 mb-12 justify-center">
+              <div className="w-full lg:w-3/12 px-4 text-center">
+                <div className="text-gray-900 p-3 w-12 h-12 shadow-lg rounded-full bg-white inline-flex items-center justify-center">
+                  <i className="fas fa-medal text-xl"></i>
+                </div>
+                <h6 className="text-xl mt-5 font-semibold text-white">
+                  Excelent Services
+                </h6>
+                <p className="mt-2 mb-4 text-gray-500">
+                  Some quick example text to build on the card title and make up
+                  the bulk of the card's content.
+                </p>
+              </div>
+              <div className="w-full lg:w-3/12 px-4 text-center">
+                <div className="text-gray-900 p-3 w-12 h-12 shadow-lg rounded-full bg-white inline-flex items-center justify-center">
+                  <i className="fas fa-poll text-xl"></i>
+                </div>
+                <h5 className="text-xl mt-5 font-semibold text-white">
+                  Grow your market
+                </h5>
+                <p className="mt-2 mb-4 text-gray-500">
+                  Some quick example text to build on the card title and make up
+                  the bulk of the card's content.
+                </p>
+              </div>
+              <div className="w-full lg:w-3/12 px-4 text-center">
+                <div className="text-gray-900 p-3 w-12 h-12 shadow-lg rounded-full bg-white inline-flex items-center justify-center">
+                  <i className="fas fa-lightbulb text-xl"></i>
+                </div>
+                <h5 className="text-xl mt-5 font-semibold text-white">
+                  Launch time
+                </h5>
+                <p className="mt-2 mb-4 text-gray-500">
+                  Some quick example text to build on the card title and make up
+                  the bulk of the card's content.
+                </p>
+              </div>
+            </div>
+            <div className="flex flex-col mt-20 bg-blue-400">
+              <div className="m-auto mt-10 w-96">
+                  <label
+                    className="block text-white text-xs font-bold mb-2"
+                    htmlFor="full-name"
+                  >
+                    Subscribe!
+                  </label>
+                  <input
+                    type="text"
+                    className="border-0 px-3 py-3 placeholder-gray-400 text-gray-700 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full"
+                    placeholder="Full Name"
+                    style={{ transition: "all .15s ease" }}
+                  />
+              </div>
+              <div className="text-center mt-6 mb-10">
+                  <button
+                          className="bg-gray-700 text-white active:bg-gray-700 text-sm font-bold uppercase px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1"
+                          type="button"
+                          style={{ transition: "all .15s ease" }}
+                        >
+                          Send Message
+                  </button>
+              </div>
+             </div>
+          </div>
+        </section>
+      </main>
+    </Layout>
+  );
+};
+
+export default IndexPage;

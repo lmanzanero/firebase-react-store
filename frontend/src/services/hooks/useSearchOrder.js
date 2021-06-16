6053f6dc0118b47e78252b3df58e4a04a7acc914
Luis Manzanero <manzanero.luis1995@gmail.com>
2021-06-17 03:23:01 +0800

    added endpoint and function to fetch order with hook

--- frontend/src/services/hooks/useSearchOrder.js
+++ frontend/src/services/hooks/useSearchOrder.js
@@ -4,23 +4,25 @@ export default function useSearchOrder(referenceNumber) {
   const [loading, setLoading] = useState(false);
   const [isValidRefNum, setIsValidRefNum ] = useState(false);
   const [data, setData ] = useState(null);
+  const [error, setError] = useState('')
 
   useEffect(() => {
     if(!referenceNumber) return  
   }, [referenceNumber])
 
-  async function searchWithReferenceNumber(){
-     try {
-      console.log("hello search order")
-      setLoading(true);
-      setTimeout(() => {  
+  async function searchWithReferenceNumber() {  
+    try {
+    setLoading(true);
+       //send referenceNumber values to api
+    const results = await fetch(`https://us-central1-nancy-s-jewerly.cloudfunctions.net/api/order/0AGHQE7O`)
+    const data = await results.json();  
+      console.log(data);
       setLoading(false);
-      setData("hello") 
-      setIsValidRefNum(true);  
-      }, 3000);
-     } catch (error) {
-       
-     }
+      setData(data);
+      setIsValidRefNum(true); 
+    } catch (error) {
+      setError(error)
+    }
   }
 
   return { loading, isValidRefNum, data, searchWithReferenceNumber }

619d7e69f812a9b68158a1a807fca620ff2baf4a
Luis Manzanero <manzanero.luis1995@gmail.com>
2021-06-17 02:37:35 +0800

    added search context to use all state from useSerachHook

---
+++ frontend/src/services/context/SearchContext.js
@@ -0,0 +1,14 @@
+import React, { createContext, useState } from 'react'
+import useSearchOrder from '../hooks/useSearchOrder';
+
+
+export const SearchContext = createContext();
+
+export const SearchProvider = ({children}) => {
+  const { loading, isValidRefNum, data, isDisabled, referenceNumber, setReferenceNumber, searchWithReferenceNumber } = useSearchOrder();  
+  return (
+    <SearchContext.Provider value={[loading, isValidRefNum, data, isDisabled, referenceNumber, setReferenceNumber, searchWithReferenceNumber]}> 
+      {children}
+    </SearchContext.Provider>
+  );
+}
